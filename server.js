9d27490be1db650d8947bb55f270e0fc73502825
Seyi Obaweya <seyi_obaweya@yahoo.com>
2020-04-20 05:04:01 +0800

    Created env variables

--- server.js
+++ server.js
@@ -3,15 +3,12 @@ const { init } = require('./utils/init')
 const updateData = require('./utils/app')
 
 
-// init()
-//     .then(data => {
-//         console.log('Cron Worker Process Running');
-//         cron.schedule('* * * * *', () => {
-//             console.log('Checking For Updates on server');
-//             updateData()
-//                 .then(console.log('done'))
-//         })
-// });
-
-updateData()
-    .then(console.log('done'))
+init()
+    .then(data => {
+        console.log('Cron Worker Process Running');
+        cron.schedule('0 * * * *', () => {
+            console.log('Checking For Updates on server');
+            updateData()
+                .then(console.log('done'))
+        })
+});
